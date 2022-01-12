6fc9539c41ee15d06189dd31f2f83829bda098b8
Asel Atakeev <aselya.atakeeva@gmail.com>
2022-01-11 22:46:22 -0800

    Added "Main, Directory, PlaceInfo Components"

---
+++ shared/places.js
@@ -0,0 +1,34 @@
+export const PLACES = [
+    {
+        id: 0,
+        name: "Issyk-Kul lake",
+        image: "/components/images/issyk-kulV8PIvXjVqrk-unsplash.jpg",
+        elevation: 1233,
+        featured: false,
+        description: "Issyk-Kul Тhе Pearl оf the Tien-ShanAs a gold dish inserted in sharp rocks lay a lake.From the Sounh this basin is locked bу а chain оf snow glands."
+    },
+    {
+        id: 1,
+        name:"Ala-Kul Lake ",
+        image: "/components/images/ej-wolfson-jTO7daIRh0U-unsplash.jpg",
+        elevation: 877,
+        featured: false,
+        description: "Ala-Kul lake is one of the most places in Kyrgyzstan. It is in the mountains at an altitude of more than 3,500 m above sea level."
+    },
+    {
+        id: 2,
+        name:"Kakshaal Too",
+        image: "/components/images/mountain-3700494_1920.jpg",
+        elevation: 2901,
+        featured: false,
+        description: "Kakshaal Too is a large mountain range in the Central Tien-Shan. It stretches for a length of 582 km between Kyrgyzstan and China."
+    },
+    {
+        id: 3,
+        name:"Burana Tower",
+        image: "/components/images/kyrgyzstan-g185a4d21d_1920.jpg",
+        elevation: 42,
+        featured: true,
+        description: "The minaret was built in the 11th century in the medieval town of Balasagun, capital of the mighty Karakhanid empire."
+    }
+];
